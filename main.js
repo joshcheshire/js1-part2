2f09017c0fc2d3ea75d4c95cb7e2014c1ed4036b
Student <student@RU-017.local>
2015-09-07 14:59:51 -0600

    completedcommit

---
+++ main.js
@@ -0,0 +1,44 @@
+//var name= prompt("What is your name?");
+
+//alert("Hello " + name);
+
+
+//Loop Practice//
+
+
+
+// for(var i = 0; i <= 500; i+= 100);{
+// 	console.log(i);
+// }
+
+
+// for (var i =1; i < 128; i*=2){
+// 	console.log(i);
+// }
+
+ // for(var i = 1; i<4; i++){
+ // 	for(var j=1; j<4; j++){
+ // 		console.log(i);
+	// }
+
+ // }
+
+ // for(var i=0; i < 12; i+=2){
+ // 	console.log(i);
+ // }
+
+// for(var i=3; i<18; i+=3){
+// 	console.log(i);
+// }
+
+
+// for(var i = 9; i >= 0; i--){
+// 	console.log(i);
+// }
+
+
+// for(var i=0; i<4; i++){
+// 	for(var j=0; j<4; j++){
+// 		console.log(i)
+// 	}
+// }
